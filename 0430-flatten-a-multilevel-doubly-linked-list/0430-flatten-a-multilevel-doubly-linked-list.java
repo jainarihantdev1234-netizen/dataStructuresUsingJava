d30cf5f9c637eaeab037d9cc3c81a29d218a10e7
Arihant jain <jainarihantdev1234@gmail.com>
2026-08-26 10:11:16 +0530

    Time: 0 ms (100%), Space: 43.3 MB (74.66%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -10,43 +10,13 @@ class Node {
 
 class Solution {
 public Node update(Node temp){
-    // if(temp == null){
-    //     return temp;
-    // }
-    // if(temp.next == null){
-    //     if(temp.child != null){
-    //         temp.next = temp.child;
-    //         temp.child.prev = temp;
-    //         temp.child = null;
-    //         return update(temp.next);
-    //     }
-    // }
-    // Node temp2 = temp;
-    // while(temp != null && temp.next != null){
-    //     if(temp.child != null){
-    //         temp2 = temp.next;
-    //         temp.next = temp.child;
-    //         temp.child.prev = temp;
-    //         temp.child = null;
-    //         Node temp1 = update(temp.next);
-    //         temp1.next = temp2;
-    //         temp2.prev = temp1;
-    //         temp = temp2;
-    //     }
-    //     else{
-    //         temp = temp.next;
-    //     }
-    // }
-    // if(temp != null && temp.child != null){
-    //     return update(temp);
-    // }
-    // return temp;
+    
     if(temp == null){
         return temp;
     }
     
     Node temp2 = temp;
-    while(temp != null && temp.next != null){
+    while(temp.next != null){
         if(temp.child != null){
             temp2 = temp.next;
             temp.next = temp.child;
